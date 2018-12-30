dc50f3d60649b1c127285741610d5584de8450d1
Tyrik Gibbs <tyrikgibbs@outlook.com>
2018-12-30 02:07:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def dsplay_rainbow(colors)
+def display_rainbow(colors)
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
